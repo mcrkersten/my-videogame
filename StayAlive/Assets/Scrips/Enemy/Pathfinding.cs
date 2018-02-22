06bd58209c250a6eefadb2660f6d92704ebcbef3
Max Kersten <max-kersten@live.nl>
2018-02-22 01:30:36 +0100

    [\] Ally AI working

--- StayAlive/Assets/Scrips/Enemy/Pathfinding.cs
+++ StayAlive/Assets/Scrips/Enemy/Pathfinding.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Pathfinding : MonoBehaviour {
     public Transform[] target;
     public float speed;
+    public Animator animator;
 
     private int current;
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.z != target[current].position.z && transform.position.x != target[current].position.x)
-        {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("CageEnd")) {
             Vector3 pos = Vector3.MoveTowards(transform.position, new Vector3(target[current].position.x, transform.position.y, target[current].position.z), speed);
             GetComponent<Rigidbody>().MovePosition(pos);
         }
-        else
+
+        Debug.DrawLine(target[current].position, transform.position, Color.green);
+
+        if (transform.position.z == target[current].position.z && transform.position.x == target[current].position.x)
         {
             current = (current +1) % target.Length;
-            print(" next Target");
+            print(current);
         }
 	}
 }
